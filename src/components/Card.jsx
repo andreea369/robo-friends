33042dd3160b6131289f15d66fcca1cc84077377
andreeafarcas369 <andreeafarcas369@yahoo.com>
2025-11-28 13:58:14 +0200

    changed some classes for the Cards and cardList

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card= (props) =>{
     return (
         <div className='card-color rounded cursor-pointer text-center hover:from-gray-600  hover:to-white hover:shadow-lg hover:scale-105 transform transition duration-500 ease-in-out'>
-            <img className= 'object-cover' src={`https://robohash.org/${props.id}?200x200`} alt='robo'></img>
+            <img className= 'object-cover img_card' src={`https://robohash.org/${props.id}?200x200`} alt='robo'></img>
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -3,7 +3,7 @@ import Card from './Card'
 
 const CardList= (props)=>{
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 lg:grid-cols-3 xl:grid-cols-4 p- px-10">
+        <div className=" grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 px-10">
             {
                 props.robots.map((user,i)=>{
                     return (
